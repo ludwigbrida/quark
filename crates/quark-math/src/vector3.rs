cf28caf08f490bdbca1c12aba7ef5b913c81b4df
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-09-21 16:27:24 +0200

    feat(math): implement cross product for vector3

--- crates/quark-math/src/vector3.rs
+++ crates/quark-math/src/vector3.rs
@@ -10,6 +10,14 @@ impl Vector3 {
   pub fn dot(self, other: Self) -> f32 {
     self.x * other.x + self.y * other.y + self.z * other.z
   }
+
+  pub fn cross(self, other: Self) -> Self {
+    Self {
+      x: self.y * other.z - self.z * other.y,
+      y: self.z * other.x - self.x * other.z,
+      z: self.x * other.y - self.y * other.x,
+    }
+  }
 }
 
 impl Sub for Vector3 {
