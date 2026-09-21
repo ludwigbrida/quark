4d0847eb337d3408b047eda5434ab12289ad6251
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-09-21 16:15:09 +0200

    feat(math): implement sub trait for vector3

--- crates/quark-math/src/vector3.rs
+++ crates/quark-math/src/vector3.rs
@@ -1,5 +1,19 @@
+use std::ops::Sub;
+
 pub struct Vector3 {
   pub x: f32,
   pub y: f32,
   pub z: f32,
+}
+
+impl Sub for Vector3 {
+  type Output = Self;
+
+  fn sub(self, rhs: Self) -> Self::Output {
+    Self {
+      x: self.x - rhs.x,
+      y: self.y - rhs.y,
+      z: self.z - rhs.z,
+    }
+  }
 }
