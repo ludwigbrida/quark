d3d331b94bf5164dddfc346cae22cba5e0990cf7
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-09-21 16:55:00 +0200

    feat(math): implement identity const for matrix4

--- crates/quark-math/src/matrix4.rs
+++ crates/quark-math/src/matrix4.rs
@@ -6,4 +6,11 @@ impl Matrix4 {
   pub const fn new(data: [[f32; 4]; 4]) -> Self {
     Self { data }
   }
+
+  pub const IDENTITY: Self = Self::new([
+    [1.0, 0.0, 0.0, 0.0],
+    [0.0, 1.0, 0.0, 0.0],
+    [0.0, 0.0, 1.0, 0.0],
+    [0.0, 0.0, 0.0, 1.0],
+  ]);
 }
